2d5d41644446349eb5c31ebf31758f8a15f60cc1
zbashay <ronatorino@gmail.com>
2020-09-27 12:05:48 -0400

    first commit

---
+++ Exercise1.java
@@ -0,0 +1,9 @@
+public class Exercise1{
+    public static void main(String[] args) {
+        System.out.println("H E L L O");
+        System.out.println("E       A");
+        System.out.println("L       P");
+        System.out.println("L       C");
+        System.out.println("O A P C S");
+    }
+}
